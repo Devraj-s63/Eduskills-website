2ed60e54a7f9714eb99f2d61ca5120fab8869dee
Devraj-s63 <ds8094848310@gamil.com>
2025-09-05 15:03:24 +0530

    update

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,13 +1,16 @@
-import mysql from "mysql2/promise";
-import dotenv from "dotenv";
+const { Pool } = require("pg");
 
-dotenv.config();
-
-const pool = mysql.createPool({
+const pool = new Pool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-export default pool;
+pool.connect()
+  .then(() => console.log("✅ Postgres connected"))
+  .catch(err => console.error("❌ Connection error", err));
